3153a93aa54a2607b28b0cfdfabad9d8a5d9b2db
Dereelcoder1 <dereelcoder@gmail.com>
2025-04-13 22:13:28 +0100

    testing

---
+++ calculator.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const numInput = document.getElementById('input');
+    const numButton = document.querySelectorAll("#num-button");
+
+
+    
+    
+})
